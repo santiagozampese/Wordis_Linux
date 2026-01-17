ffd114476f4ea15ebb0a3bf0ddc9a1bcbd237d72
santiagozampese <santiagozampese@gmail.com>
2026-01-17 12:07:31 -0300

    Add files via upload

---
+++ Wordis_Linux/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+caminho_relativo_app=$(find . -name "Wordis_linux")
+caminho_relativo_icon=$(find . -name "WordisIco.ico")
+caminho_app=$(readlink -f $caminho_relativo_app)
+caminho_icon=$(readlink -f $caminho_relativo_icon)
+chmod +x $caminho_app
+
+echo "[Desktop Entry]
+Name=Wordis
+Type=Application
+Terminal=true
+Exec=$caminho_app
+Icon=$caminho_icon
+Categories=Games" > Wordis.desktop
